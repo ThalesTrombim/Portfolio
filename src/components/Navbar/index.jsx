d1ed497347596249466ad4dd1404fe31e3334dd8
ThalesTrombim <thalestr@outlook.com>
2022-04-25 00:30:54 -0300

    mobile: navbar responsivo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import style from './style.module.scss';
 import { Button } from '../Button';
@@ -8,13 +8,15 @@ import { ThemeContext } from '../../contexts/ThemeContext';
 
 function Navbar () {
     const { theme, handleTheme } = useContext(ThemeContext);
+    const [ mobileMenu, setMobileMenu ] = useState(true);
+
     let widthHeight = 40;
     if(theme === 'light'){
         widthHeight = 30;
     }
     return (
         <div className={`${style.bgContainer} ${style[theme]}`}>
-            <header className={style.container}>
+            <header className={style.containerDesktop}>
                 <div className={style.navLeft}>
                     <Link  href={'/'} passHref>
                         <a className={style.logo}>
@@ -43,6 +45,75 @@ function Navbar () {
                         Contact
                     </Button>
                 </div>
+            </header>
+            <header className={style.containerMobile}>
+                {
+                    !mobileMenu ? (
+                        <>
+                            <div className={style.navLeft}>
+                                <Link  href={'/'} passHref>
+                                    <a className={style.logo}>
+                                        Thales
+                                    </a>
+                                </Link>
+                            </div>
+
+                            <div className={style.navRight}>
+                                <div onClick={() => handleTheme()} className={style.toggleImg}>
+                                    <Image src={`/icons/${theme}.png`} width={widthHeight} height={widthHeight} />
+                                </div>
+
+                                <div onClick={() => handleTheme()} className={style.toggleImg}>
+                                    <Image src={`/icons/burguer.png`} width={35} height={35} />
+                                </div>
+                                {/* <Button link={'/contact'} mode={'link'}>
+                                    Contact
+                                </Button> */}
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className={style.menuMobile}>
+                                {/* <Link  href={'/'} passHref>
+                                    <a className={style.logo}>
+                                        
+                                    </a>
+                                </Link> */}
+                                <span>MENU</span>
+
+                                <Link  href={'/'} passHref>
+                                    <a className={style.menuMobileLink}>
+                                        About
+                                    </a>
+                                </Link>
+
+                                <Link  href={'/portfolio'} passHref>
+                                    <a className={style.menuMobileLink}>
+                                        Portfolio
+                                    </a>
+                                </Link>
+
+                                <Link  href={'/contact'} passHref>
+                                    <a className={style.menuMobileLink}>
+                                        Contact
+                                    </a>
+                                </Link>
+
+                                <div className={style.iconsMenuMobile}>
+                                    <a href="https://github.com/ThalesTrombim" target={'_blank'}>
+                                        <Image src={`/icons/github.png`} width={25} height={25} />
+                                    </a>
+                                    <a href="mailto:thalestr@outlook.com" target={'_blank'}>
+                                        <Image src={`/icons/email.png`} width={30} height={30} />
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/thales-trombim/" target={'_blank'}>
+                                        <Image src={`/icons/linkedin.png`} width={25} height={25} />
+                                    </a>
+                                </div>
+                            </div>
+                        </>
+                    )
+                }
             </header>
         </div>
     )
